4279545e9ea2261d0795bd3b8ed9b7a6fca22c42
alkuzin <alkuzindev@gmail.com>
2025-07-17 11:43:26 +0300

    [feat]: added hypervisor management service crate

---
+++ nullvm-service/src/main.rs
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2025-present nullvm hypervisor project and contributors
+
+//! NullVM hypervisor management service entry point.
+
+fn main() {
+    println!("Running NullVM service");
+}
