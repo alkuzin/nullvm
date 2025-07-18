490872c9c12b3ec93abae7e474ed6ef47796f4df
alkuzin <alkuzindev@gmail.com>
2025-07-18 14:39:40 +0300

    [feat]: added set_vm_memory method

--- nullvm_service/src/vmm/vm.rs
+++ nullvm_service/src/vmm/vm.rs
@@ -3,11 +3,14 @@
 
 //! Virtual machine related declarations.
 
-use crate::vmm::{VmmError, VmmResult};
+use crate::{ManualFree, VmmError, VmmResult};
 use kvm_bindings::kvm_userspace_memory_region;
 use kvm_ioctls::{Kvm, VcpuFd, VmFd};
-use libc::{MAP_ANONYMOUS, MAP_SHARED, PROT_READ, PROT_WRITE, mmap};
-use std::ptr::null_mut;
+use libc::{
+    MAP_ANONYMOUS, MAP_FAILED, MAP_SHARED, PROT_READ, PROT_WRITE, mmap, munmap,
+};
+use nullvm_common::MMapWrapper;
+use std::{ops::Deref, ptr::null_mut};
 
 /// Virtual machine info struct.
 pub struct VirtualMachine {
@@ -17,7 +20,9 @@ pub struct VirtualMachine {
     vmfd: VmFd,
     /// Virtual CPU file descriptor.
     vcpu: VcpuFd,
-    /// Memory region of VM.
+    /// Memory allocated to VM.
+    memory: Option<MMapWrapper>,
+    /// VM's memory region info.
     mem_region: kvm_userspace_memory_region,
 }
 
@@ -25,7 +30,8 @@ impl VirtualMachine {
     /// Construct new virtual machine.
     ///
     /// # Returns
-    /// - New `VirtualMachine` object.
+    /// - New `VirtualMachine` object - in case of success.
+    /// - `Err` - otherwise.
     pub fn new() -> VmmResult<Self> {
         let kvm = Kvm::new()?;
         let version = kvm.get_api_version();
@@ -43,9 +49,52 @@ impl VirtualMachine {
             kvm,
             vmfd,
             vcpu,
+            memory: None,
             mem_region: Default::default(),
         };
+
         Ok(result)
+    }
+
+    /// Set VM's memory.
+    ///
+    /// # Parameters
+    /// - `memory_size` - given size of the memory region in bytes to allocate.
+    ///
+    /// # Returns
+    /// - `Ok`  - in case of success.
+    /// - `Err` - otherwise.
+    fn set_vm_memory(&mut self, mem_size: usize) -> VmmResult<()> {
+        if mem_size == 0 {
+            let msg = "Mapping memory size is zero".to_string();
+            return Err(VmmError::from(msg));
+        }
+
+        let addr = unsafe {
+            // Mapping protection flags.
+            let prot = PROT_READ | PROT_WRITE;
+
+            // MAP_SHARED flag ensures that both the host and guest can see the
+            // same changes to the memory.
+            // MAP_ANONYMOUS flag indicates that the mapping is not
+            // backed by any file.
+            let flags = MAP_SHARED | MAP_ANONYMOUS;
+
+            let addr = mmap(null_mut(), mem_size, prot, flags, -1, 0);
+
+            if addr == MAP_FAILED {
+                return Err(VmmError::from("Failed to map memory".to_string()));
+            }
+
+            addr
+        };
+
+        let cleanup = Box::new(move |value| unsafe {
+            munmap(value, mem_size);
+        });
+
+        self.memory = Some(MMapWrapper::new(addr, cleanup));
+        Ok(())
     }
 
     /// Set userspace memory region.
@@ -62,40 +111,28 @@ impl VirtualMachine {
         guest_paddr: u64,
         mem_size: usize,
     ) -> VmmResult<()> {
-        if mem_size == 0 {
-            let msg = "Mapping memory size is zero".to_string();
-            return Err(VmmError::from(msg));
+        self.set_vm_memory(mem_size)?;
+
+        if let Some(memory) = &self.memory {
+            let mem_region = kvm_userspace_memory_region {
+                // Memory region's unique slot number.
+                slot: 0,
+                // Flags that modify the behavior of the memory region.
+                flags: 0,
+                guest_phys_addr: guest_paddr,
+                memory_size: mem_size as u64,
+                userspace_addr: *memory.deref() as u64,
+            };
+
+            unsafe {
+                self.vmfd.set_user_memory_region(mem_region)?;
+            }
+
+            self.mem_region = mem_region;
+            return Ok(());
         }
 
-        let userspace_addr = unsafe {
-            // Mapping protection flags.
-            let prot = PROT_READ | PROT_WRITE;
-
-            // MAP_SHARED flag ensures that both the host and guest can see the
-            // same changes to the memory.
-            // MAP_ANONYMOUS flag indicates that the mapping is not
-            // backed by any file.
-            let flags = MAP_SHARED | MAP_ANONYMOUS;
-
-            mmap(null_mut(), mem_size, prot, flags, -1, 0)
-        };
-
-        let mem_region = kvm_userspace_memory_region {
-            // Memory region's unique slot number.
-            slot: 0,
-            // Flags that modify the behavior of the memory region.
-            flags: 0,
-            guest_phys_addr: guest_paddr,
-            memory_size: mem_size as u64,
-            userspace_addr: userspace_addr as u64,
-        };
-
-        unsafe {
-            self.vmfd.set_user_memory_region(mem_region)?;
-        }
-
-        self.mem_region = mem_region;
-        Ok(())
+        Err(VmmError::from("VM's memory wasn't set".to_string()))
     }
 }
 
@@ -112,7 +149,7 @@ pub mod tests {
     #[test]
     fn test_set_user_mem_region() {
         let mut vm = VirtualMachine::new().unwrap();
-        let result = vm.set_user_mem_region(0, 0x1000);
+        let result = vm.set_user_mem_region(0x1000, 0x1000);
 
         assert!(result.is_ok())
     }
