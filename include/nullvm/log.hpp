208209c6c4f55caf8afb55049b2e8d904fefbde6
alkuzin <alkuzindev@gmail.com>
2025-08-11 15:53:26 +0300

    [feat]: added log::panic() function & PanicFormat struct

--- include/nullvm/log.hpp
+++ include/nullvm/log.hpp
@@ -7,10 +7,36 @@
 #define NULLVM_LOG_HPP
 
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include <source_location>
+#include <string_view>
 #include <format>
 #include <print>
 
 namespace nullvm::log {
+
+    /// @brief Logs a custom message into string.
+    ///
+    /// @param [in] level given custom log level string.
+    /// @param [in] fmt given format string.
+    /// @param [in] args given format string arguments.
+    ///
+    /// @return Custom message log string.
+    template<typename... Args>
+    auto custom_format(
+        const std::string_view& level,
+        std::format_string<Args...> fmt,
+        Args&&... args
+    )
+    -> std::string {
+
+        const auto now = boost::posix_time::second_clock::local_time();
+        auto current_time = boost::posix_time::to_simple_string(now);
+
+        auto time_str = std::format("[{}] [{}] ", current_time, level);
+        auto log_str = std::format(fmt, std::forward<Args>(args)...);
+
+        return time_str + log_str + "\n";
+    }
 
     /// @brief Logs a custom message with a specified format and arguments.
     ///
@@ -18,12 +44,14 @@ namespace nullvm::log {
     /// @param [in] fmt given format string.
     /// @param [in] args given format string arguments.
     template<typename... Args>
-    auto custom(const std::string_view& level, std::format_string<Args...> fmt, Args&&... args) -> void {
-        const auto now = boost::posix_time::second_clock::local_time();
-        auto current_time = boost::posix_time::to_simple_string(now);
-
-        std::print("[{}] [{}] ", current_time, level);
-        std::println(fmt, std::forward<Args>(args)...);
+    auto custom(
+        const std::string_view& level,
+        std::format_string<Args...> fmt,
+        Args&&... args
+    )
+    -> void {
+        auto msg = custom_format(level, fmt, std::forward<Args>(args)...);
+        std::print("{}", msg);
     }
 
     /// @brief Logs an informational message.
@@ -62,14 +90,44 @@ namespace nullvm::log {
         custom("TEST", fmt, std::forward<Args>(args)...);
     }
 
+    /// Panic format string wrapper.
+    template <typename... Args>
+    struct PanicFormat {
+        /// Log format string.
+        std::format_string<Args...> format;
+        /// Source code location info where panic occured.
+        std::source_location location;
+
+        /// @brief Construct new PanicFormat object.
+        ///
+        /// @param str given format string.
+        /// @param loc given source code location info.
+        template <typename T>
+        consteval PanicFormat(
+            const T &str,
+            const std::source_location loc = std::source_location::current()
+        ) noexcept : format{str}, location{loc} {}
+    };
+
     /// @brief Logs a panic message and terminates process.
     ///
     /// @param [in] fmt given format string.
     /// @param [in] args given format string arguments.
     template<typename... Args>
-    auto panic(std::format_string<Args...> fmt, Args&&... args) -> void {
-        custom("PANIC", fmt, std::forward<Args>(args)...);
-        std::exit(EXIT_FAILURE);
+    [[noreturn]]
+    auto panic(
+        const PanicFormat<std::type_identity_t<Args>...>& fmt,
+        Args&&... args
+    ) -> void {
+
+        const auto& loc = fmt.location;
+
+        custom("PANIC", "Panic occured in file '{}'", loc.file_name());
+        custom("PANIC", "In function '{}'", loc.function_name());
+        custom("PANIC", "On line: {}", loc.line());
+        custom("PANIC", fmt.format, std::forward<Args>(args)...);
+
+        std::terminate();
     }
 
 }
