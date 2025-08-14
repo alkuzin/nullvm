f9c956866a90de86a9f6373c32d24f3f3f3da394
alkuzin <alkuzindev@gmail.com>
2025-08-14 12:59:21 +0300

    [feat]: added send() & recv() methods for Server

--- include/nullvm/service/server.hpp
+++ include/nullvm/service/server.hpp
@@ -6,13 +6,45 @@
 #ifndef NULLVM_SERVICE_SERVER_HPP
 #define NULLVM_SERVICE_SERVER_HPP
 
+#include <nullvm/types.hpp>
+
 namespace nullvm::service {
 
     /// Server abstract class.
     class Server {
     public:
         /// @brief Destroy Server object.
-        virtual ~Server() noexcept = 0;
+        virtual ~Server() noexcept = default;
+
+        /// @brief Initialize server.
+        ///
+        /// @return None - in case of success.
+        /// @return VmmError - otherwise.
+        virtual auto init() noexcept -> VmmResult<None> = 0;
+
+        /// @brief Send data to client.
+        ///
+        /// @param [in] fd given client connection file descriptor.
+        /// @param [in] data given sequence of bytes to send.
+        ///
+        /// @return None - in case of success.
+        /// @return VmmError - otherwise.
+        virtual auto send(i32 fd, const Bytes& data) noexcept
+        -> VmmResult<None> = 0;
+
+        /// @brief Receive data from client.
+        ///
+        /// @param [in] fd given client connection file descriptor.
+        ///
+        /// @return Received sequence of bytes - in case of success.
+        /// @return VmmError - otherwise.
+        virtual auto recv(i32 fd) noexcept -> VmmResult<Bytes> = 0;
+
+        /// @brief Run server.
+        ///
+        /// @return None - in case of success.
+        /// @return VmmError - otherwise.
+        virtual auto run() noexcept -> VmmResult<None> = 0;
     };
 
 }
